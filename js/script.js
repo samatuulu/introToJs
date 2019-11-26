1cbd1a27e4a273aa47ffbc6595e68dce5b58e08d
Bektursun <samatuulu11@gmail.com>
2019-11-26 20:58:54 +0600

    Done first task. Played with xhrHttpRequest.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let class_inside = document.getElementById('container');
+
+
+function randomTag(){
+	class_inside_2 = document.createElement('div');
+	class_inside_2.innerText = Math.floor((Math.random() * (20 + 1)));
+	class_inside.appendChild(class_inside_2);
+
+
+};
+
+var intervalId = setInterval(randomTag, 3000);
